33e1877dabfd7d7c070e33af78c93f26ec97db8c
BYTE90 <digarsingh2004@gmail.com>
2024-08-31 21:16:07 +0530

    sum

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    int y;
+    int a;
+    cout<<"enter the first number";
+    cin>>x;
+
+    cout<<"enter the second number";
+    cin>>y;
+
+    a = x+y;
+
+    cout<<"sum is : "<<a;
+
+    return 0;
+
+}
