d42d27b7ec584edc3b87f38969771a68ef3d7db5
BYTE90 <digarsingh2004@gmail.com>
2024-08-31 21:24:55 +0530

    sum

---
+++ rows.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows;
+    cout<<"enter the number of rows ";
+    cin>>rows;
+
+    for (int i=1;i<=rows;i++)
+    {
+        for(int j;j=1;j<=i;j++)
+    }
+    cout<<"*";
+    cin>>"\n";
+
+    return 0;
+
+}
