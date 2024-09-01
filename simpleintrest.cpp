a6916ad30dde8229d5b40c4692538cf0a9d34a16
BYTE90 <digarsingh2004@gmail.com>
2024-09-01 20:28:06 +0530

    simple intrest

---
+++ simpleintrest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float p;
+    float r;
+    float t;
+    float a;
+
+cout<<"enter p : ";
+    cin>>p;
+cout<<"enter r : ";
+    cin>>r;
+cout<<"enter t : ";
+    cin>>t;
+
+    a= (p*r*t)/100;
+cout<<a; 
+
+
+
+
+    return 0;
+
+}
