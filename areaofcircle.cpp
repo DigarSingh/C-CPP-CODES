40de9128587450626bf17cb2f4c9bdb64d12ef42
BYTE90 <digarsingh2004@gmail.com>
2024-09-01 20:17:49 +0530

    area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float r;
+    float a;
+    cout<<"enter r : ";
+    cin>>r;
+
+    a=3.14*r*r;
+    cout<<"area of circle is: "<<a;
+    return 0;
+
+}
