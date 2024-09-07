ec248712c96c65da087c444b266c1215dc510c84
BYTE90 <digarsingh2004@gmail.com>
2024-09-07 23:33:28 +0530

    3 digit number

---
+++ three_digit_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter three digit number";
+    cin>>n;
+
+    if(n>99 and n<1000)
+        cout<<"three digit number";
+    else
+        cout<<"not three digit number";
+
+}
