7a5bae7b67acd6f664a934b1ad993609c43b90a1
BYTE90 <digarsingh2004@gmail.com>
2024-09-07 23:33:46 +0530

    Greater number

---
+++ greater_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the first number : ";
+    cin>>a;
+    cout<<"enter the second number  ";
+    cin>>b;
+    cout<<"enter the third number : ";
+    cin>>c;
+
+    if(a>b)
+        if(a>c)
+            cout<<a<<" is greatest";
+        else
+            cout<<c<<" is greatest";
+    
+    else
+        if(b>c)
+            cout<<b<<" is greatest";
+        else    
+            cout<<c<<" is greatest";
+
+}
