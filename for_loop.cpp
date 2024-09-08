fefe47b1b1b12cc83fb12eb55ed7423c03e96c84
BYTE90 <digarsingh2004@gmail.com>
2024-09-08 18:59:05 +0530

    For Loop

---
+++ for_loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i,a;
+    cout<<"enter the number : ";
+    cin>>a;
+    for(i=1;i<=a; i++) {
+        cout<<"Hello World"<<endl;
+    }
+}
