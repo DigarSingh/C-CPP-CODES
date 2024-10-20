baa059b85a9615dc62912d86f2883befda6ec799
BYTE90 <digarsingh2004@gmail.com>
2024-10-20 16:54:43 +0530

    c code

---
+++ CODE OF C/CH-4 Pattern Printing/alphabet_triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter the no. of rows : ");
+    scanf("%d",&n);
+
+    char ch;
+
+    for(int i=1;i<=n;i++){
+        int a=0;
+        for(int j=1;j<=i;j++){
+            printf("%c ",a+65);
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
