8afe7b5aeea44f456913e5903f785b6158c30563
BYTE90 <digarsingh2004@gmail.com>
2024-10-22 00:06:31 +0530

    c code

--- CODE OF C/CH-4 Pattern Printing/0_1_triangle.c
+++ CODE OF C/CH-4 Pattern Printing/0_1_triangle.c
@@ -4,10 +4,16 @@ int main () {
     int n;
     printf("enter the no. : ");
     scanf("%d",&n);
-
+    int a;
     for(int i=1;i<=n;i++){
+        if(i%2!=0) a=1;
+        else a=0;
         for(int j=1;j<=i;j++){
-            for 
+            printf("%d ",a);
+            if(a==0) a=1;
+            else a=0;
         }
+        printf("\n");
     }
+    return 0;
 }
