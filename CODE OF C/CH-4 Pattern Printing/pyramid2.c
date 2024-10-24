f372df39f1b6f4cdd44a9f4239fa756126984303
BYTE90 <digarsingh2004@gmail.com>
2024-10-24 21:19:22 +0530

    c code

--- CODE OF C/CH-4 Pattern Printing/pyramid2.c
+++ CODE OF C/CH-4 Pattern Printing/pyramid2.c
@@ -6,18 +6,17 @@ int main() {
     scanf("%d",&n);
 
     int nst=1;
-    int nsp=3;
 
     for(int i=1;i<=n;i++){
-        for(int k=1;k<=nsp;k++){
+        for(int k=1;k<=n-i;k++){
             printf(" ");
         }
-        nsp=nsp-1;
+    }
+    return 0;
+} 
+
         for(int j=1;j<=nst;j++){
             printf("*");
         }
         nst=nst+2;
         printf("\n");
-    }
-    return 0;
-} 
