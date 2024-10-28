b8ca559ed662813278ab76c6e5ddac66b3699b92
BYTE90 <digarsingh2004@gmail.com>
2024-10-28 12:41:40 +0530

    c code

--- CODE OF C/CH-4 Pattern Printing/up_arror_star.c
+++ CODE OF C/CH-4 Pattern Printing/up_arror_star.c
@@ -4,19 +4,29 @@ int main () {
     int n;
     printf("enter the no. of rows : ");
     scanf("%d",&n);
-    int nsp = 0;
+    int nsp = 1;
     int nst = n;
     
-
+    for(int i=1;i<=2*n+1;i++){
+            printf("*");
+        }
+        printf("\n");
     for(int i=1;i<=n;i++){
+        int a=1;
+        for(int k=1;k<=nst;k++){
+            printf("*");
+            a++;
+        }
         for(int j=1;j<=nsp;j++){
             printf(" ");
+            a++;
         }
-        nsp++;
         for(int k=1;k<=nst;k++){
-            printf("*");     
+            printf("*"); 
+            a++;    
         }
         nst--;
+        nsp+=2;
         printf("\n");
     }
     return 0;
