9108ae79ff4a9f73ece090409203e6d52523d34e
BYTE90 <digarsingh2004@gmail.com>
2024-10-29 16:21:48 +0530

    c code

---
+++ CODE OF C/CH-5 Function & Pointers/greet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // greet();
+    for(int i=1;i<=10;i++){
+        greet();
+    }
+    return 0;
+}
+
+void greet(){
+    printf("Good Morning\n");
+
+    return;
+}
