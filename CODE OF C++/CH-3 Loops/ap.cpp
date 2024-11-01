9019c6975278edd7eb8f2c87f5636b55323c9b05
BYTE90 <digarsingh2004@gmail.com>
2024-11-01 15:16:50 +0530

    c++ codes

--- CODE OF C++/CH-3 Loops/ap.cpp
+++ CODE OF C++/CH-3 Loops/ap.cpp
@@ -4,10 +4,11 @@ int main() {
     int n;
     cout<<"enter the number : ";
     cin>>n;
-    //4,7,10,13..
-    int a= 4;
+    //100 ,97,94 .....
+    int a= 100;
     for(int i=1;i<=n; i++){ 
         cout<<a<<" ";
-        a=a+3;
+        a=a-3;
     }
+    return 0;
 }
