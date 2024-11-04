72cb5b329487005dd62e2f61bae4bb620779acfc
BYTE90 <digarsingh2004@gmail.com>
2024-11-04 14:35:16 +0530

    square root of number

--- CODE OF C++/CH-5 Function & Pointers/square_root.cpp
+++ CODE OF C++/CH-5 Function & Pointers/square_root.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
      int a;
-     cout << "Enter a number: ";
-     cin >> a;
+     cout<<"Enter a number: ";
+     cin>>a;
 
      int root= sqrt(a);
-     cout << "The square root of "<<a<<" is "<<root<<endl;
+     cout<<"The square root of "<<a<<" is "<<root<<endl;
 
      return 0;
 }
