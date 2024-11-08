970ceb42ecb37edac299667545f6be69fe8617b8
BYTE90 <digarsingh2004@gmail.com>
2024-11-08 20:27:50 +0530

    function (min and max)

---
+++ CODE OF C++/CH-5 Function & Pointers/min_max.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+     int n = min(5, 10);
+     int m = max(5, 10);
+     printf("min : %d\n", n);
+     printf("max : %d\n",m);
+     return 0;
+}
