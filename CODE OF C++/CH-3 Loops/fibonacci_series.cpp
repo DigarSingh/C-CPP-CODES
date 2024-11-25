7a9ff0e4e817df7d6348b21b41c5284b5fe86bd7
BYTE90 <digarsingh2004@gmail.com>
2024-11-25 14:26:33 +0530

    fibonacci_series

--- CODE OF C++/CH-3 Loops/fibonacci_series.cpp
+++ CODE OF C++/CH-3 Loops/fibonacci_series.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-     int a=0,b=1,n;
+     int a=1,b=1,n;
      cout<<"Enter the number of terms : ";
      cin>>n;
-     int sum;
-     for(int i=1;i<=n;i++){
+     int sum=0;
+     for(int i=1;i<=n-2;i++){
          cout<<a<<" ";
          sum = a+b;
          a=b;
          b=sum;
      }
+     cout<<endl<<b;
      return 0;
 }
