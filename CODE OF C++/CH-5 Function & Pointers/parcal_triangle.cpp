f4db6e767b32ba1558c9fa995379167f17b6ec3d
BYTE90 <digarsingh2004@gmail.com>
2024-12-15 16:50:14 +0530

    Parcal Triangle

--- CODE OF C++/CH-5 Function & Pointers/parcal_triangle.cpp
+++ CODE OF C++/CH-5 Function & Pointers/parcal_triangle.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int x){
+int fact(int x){
      int fact=1;
      for(int i=1;i<=x;i++){
-          fact=fact*i;  
+          fact*=i;  
      }
      return fact;
-}     
-int combination(int n, int r){
-     int ncr=factorial(n)/(factorial(r)*factorial(n-r));
+}
+int comb(int n,int r){
+     int ncr=fact(n)/(fact(r)*fact(n-r));
      return ncr;
 }
 int main() {
@@ -17,12 +17,12 @@ int main() {
      cout << "Enter the number of rows: ";
      cin>>n;
 
-     for(int i=0;i<=n;i++){
+     for(int i=0;i<=n;i++){ //space
           for(int j=0;j<=n-i;j++){
                cout<<" ";
           }
-          for(int j=0;j<=i;j++){
-               int icj = combination(i,j);
+          for(int j=0;j<=i;j++){ 
+               int icj = comb(i,j);
                cout<<icj<<" ";
           }
           cout<<endl;
