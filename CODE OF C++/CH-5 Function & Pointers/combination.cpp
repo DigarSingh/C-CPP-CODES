87b58530ce1f45270f147afc3542e86b59dfe61f
BYTE90 <digarsingh2004@gmail.com>
2024-12-15 16:44:27 +0530

    Combination

--- CODE OF C++/CH-5 Function & Pointers/combination.cpp
+++ CODE OF C++/CH-5 Function & Pointers/combination.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int factrioal(int x){
+int fact(int x){
      int fact=1;
      for(int i=1;i<=x;i++){
-          fact=fact*i;  
+          fact*=i;  
      }
      return fact;
 }
-
+int comb(int n,int r){
+     int ncr=fact(n)/(fact(r)*fact(n-r));
+     return ncr;
+}
 int main() {
      int n;
-     cout<<"Enter the value of n and r : ";
+     cout<<"Enter the value of n : ";
      cin>>n;
-     int ncr=factrioal(n);
-     printf("factrioal of%d",ncr);
+     int r;
+     cout<<"Enter the value of r : ";
+     cin>>r;
+     int nfact = fact(n);
+     int rfact = fact(r);
+     int nrfact = fact(n-r);
+     int ncr = comb(n,r);
+     cout<<ncr;
      return 0;
 }
