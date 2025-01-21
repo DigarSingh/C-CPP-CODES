fcbd834753fe887902f593699fc0f6a05b5aa739
BYTE90 <digarsingh2004@gmail.com>
2025-01-21 14:32:32 +0530

    --

--- CODE OF C++/CH-6 Array/array1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-     int arr[7] = {10, 20, 30, 40, 50, 60, 70};
-     for(int i=0;i<6;i++){
-           cout << arr[i] << ",";
-     }
-     return 0;
-}
