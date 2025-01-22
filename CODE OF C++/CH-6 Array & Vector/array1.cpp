9f354eb596745a09f27cc347f21b2bbab51f2de5
BYTE90 <digarsingh2004@gmail.com>
2025-01-22 21:22:21 +0530

    c++ code

---
+++ CODE OF C++/CH-6 Array & Vector/array1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int arr[7] = {10, 20, 30, 40, 50, 60, 70};
+     for(int i=0;i<6;i++){
+           cout << arr[i] << ",";
+     }
+     return 0;
+}
