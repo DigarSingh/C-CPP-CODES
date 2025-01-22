394a35295a9d50bc994ec198cf0183359f24f53f
BYTE90 <digarsingh2004@gmail.com>
2025-01-22 21:52:01 +0530

    Operations Of Vector

--- CODE OF C++/CH-6 Array & Vector/operationsOfVector.cpp
+++ CODE OF C++/CH-6 Array & Vector/operationsOfVector.cpp
@@ -6,20 +6,31 @@ int main() {
      vector <int> v;
      
      v.push_back(1);
+     cout<<v.size()<<endl;
      v.push_back(2);
+     cout<<v.size()<<endl;
      v.push_back(3);
+     cout<<v.size()<<endl;
      v.push_back(4);
+     cout<<v.size()<<endl;
      v.push_back(5);
+     cout<<v.size()<<endl;
      v.push_back(6);
+     cout<<v.size()<<endl;
      v.push_back(7);
+     cout<<v.size()<<endl;
      v.push_back(8);
+     cout<<v.size()<<endl;
      v.push_back(9);
+     cout<<v.size()<<endl;
      v.push_back(10);
+     cout<<v.size()<<endl;
 
      for(int i=0;i<=v.size();i++){
           cout<<v[i]<<" ";
      }
      cout<<endl;
+     v.pop_back();
      v.pop_back();
      v.pop_back();
 
