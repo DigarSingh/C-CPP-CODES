b7de9708087fb84a4bc400607014e27b3990de92
BYTE90 <digarsingh2004@gmail.com>
2025-02-04 19:44:59 +0530

    Reverse of an Array

---
+++ CODE OF C++/CH-6 Array & Vector/ReverseArray1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void printvector(vector<int> &a){
+     for(int i=0;i<a.size();i++){
+          cout<<a[i]<<" ";
+     }
+     cout<<endl;
+}
+void reverse(vector<int>& v){
+     int i = 0;
+     int j = v.size()-1;
+     while(i<=j){
+     int temp = v[i];
+     v[i] = v[j];
+     v[j] = temp;
+
+     i++;
+     j--;
+     }
+     return;
+}
+void reversepart(int i,int j,vector<int>&v){
+     while(i<=j){
+     int temp = v[i];
+     v[i] = v[j];
+     v[j] = temp;
+
+     i++;
+     j--;
+     }
+     return;
+}
+
+int main(){
+     vector<int> v;
+     v.push_back(2);
+     v.push_back(3);
+     v.push_back(5);
+     v.push_back(7);
+     v.push_back(9);
+     v.push_back(1);
+
+     printvector(v);
+     reverse(v);
+     //reversepart(0,5,v);
+     printvector(v);
+
+     return 0;
+
+}
